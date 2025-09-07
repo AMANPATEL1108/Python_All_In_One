e757c4a7d6fd5f28829052efa51fa9f6e5ef9b12
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-09-07 20:27:09 +0530

    Practice of list,tuple,dictonary and set Completed

---
+++ Practice/Practice1.py
@@ -0,0 +1,92 @@
+#answer 1-----------------List Questions-------------------------------
+# list1=[]
+# list1.append("Mango")
+# list1.append("Banana")
+# list1.append("Apple")
+# list1.append("Pineapple")
+# list1.append("Gvava")
+# #answer 2
+# print(list1[3])
+# #answer 3
+# list1.remove("Banana")
+# print(list1)
+# #answer 4
+# print(len(list1))
+# #answer 5
+# list2=[1,2,3,4,5]
+# print(list2[0]+list2[1]+list2[2]+list2[3]+list2[4])
+
+#answer 1-----------------Tuple Questions-------------------------------
+# tuple1=(1,2,3,4,5)
+# print(tuple1)
+# #answer 2
+# print(tuple1[len(tuple1)-1])
+# #asnwer 3
+# #we can not chnage because that is immutable
+# #answer 4
+# #Directly We can not get First getting all elements from list after at a time add that all because after that typle not chnage because that is immutable
+# list3=[1,2,3,4,5]
+# e1=list3[0]
+# e2=list3[1]
+# e3=list3[2]
+# e4=list3[3]
+# e5=list3[4]
+# tuple2=(e1,e2,e3,e4,e5)
+# print(tuple2)
+# print(type(tuple2))
+# #answer 5
+# name=input("Enter a Name:")
+# age=int(input("ENter age:"))
+# city=input("ENter a city:")
+# tuple3=(name,age,city)
+# print(type(tuple3))
+
+#answer 1-----------------Dictonary Questions-------------------------------
+# dic1 = {"name": "Aman", "age": 21,"marks":56}
+# #answer 2
+# print(dic1["name"])
+# #answer 3
+# dic1["grade"]="A"
+# print(dic1)
+# #answer 4
+# dic1.pop("marks")
+# print(dic1)
+# #answer 5
+# for key in dic1:
+#         print(key,dic1[key])
+
+#answer 1-----------------Set Questions-------------------------------
+
+# s1={1,2,62,3,4,5}
+# #answer 2
+# s1.add(10)
+# #answer 3
+# s1.remove(2)
+# #answer 4
+# s11={1,2,3}
+# s22={3,4,5}
+# print(s11.union(s22))
+# print(s1)
+# #answe 5
+# s11.add(3)
+#If already Present then replace then that is not duplicate in that
+
+#answer 1-----------------Mixed Questions-------------------------------
+# answer 1
+# duplicate allows->list,tuple
+#not duplicate->dictonary,set
+#answer 2
+# dictonary is a key value paris data structure
+#answer 3
+#i have a store roll number without duplciate->set i can use 
+#answer 4
+# if i want to order and unchangebal then i can use a Set data structure
+#answer 5
+# empty list
+listempty=[]
+#empty tuple
+emptytuple=()
+#empty dictonary
+emptydic={}
+#empty set
+emptyset=set()
